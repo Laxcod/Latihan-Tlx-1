230259dc5998b0291b964a831b20cb2a5ecde57a
Laxcod <90342920+Laxcod@users.noreply.github.com>
2021-09-19 19:50:09 +0700

    Add files via upload

---
+++ BurungBeoTlx.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class BurungBeoTlx {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String beo = scan.nextLine();
+        System.out.println(beo);
+
+        scan.close();
+    }
+
+}
