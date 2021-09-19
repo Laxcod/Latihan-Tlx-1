cf5cdafb5160ef5b78f2879a2455f43ae25f7ba6
Laxcod <90342920+Laxcod@users.noreply.github.com>
2021-09-19 19:56:58 +0700

    Update BurungBeoTlx.java

--- BurungBeoTlx.java
+++ BurungBeoTlx.java
@@ -1,11 +1,12 @@
 import java.util.Scanner;
+public class BurungBeoTlx 
 
-public class BurungBeoTlx {
-    public static void main(String[] args) {
+{
+    public static void main(String[] args) 
+    {
         Scanner scan = new Scanner(System.in);
         String beo = scan.nextLine();
         System.out.println(beo);
-
         scan.close();
     }
 
