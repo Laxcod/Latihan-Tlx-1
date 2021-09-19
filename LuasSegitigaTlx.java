03c3a5779f4c4c0cb80ec12c7704c6ed58f50383
Laxcod <90342920+Laxcod@users.noreply.github.com>
2021-09-19 19:58:01 +0700

    Update LuasSegitigaTlx.java

--- LuasSegitigaTlx.java
+++ LuasSegitigaTlx.java
@@ -1,15 +1,15 @@
-
 import java.util.Scanner;
-public class LuasSegitigaTlx {
-    public static void main (String [] args){
+public class LuasSegitigaTlx 
+
+{
+    public static void main (String [] args)
+    
+    {
      Scanner luas = new Scanner(System.in);
      double A = luas.nextDouble();
      double T = luas.nextDouble();
      double Segitiga = A*T/2; 
      System.out.printf("%.2f" , Segitiga);
-
-
-
     }
     
 }
